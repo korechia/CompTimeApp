394a6853c805b9f5fccbc3d4e81f6459e8255939
korechia <orechia_kathleen@wheatoncollege.edu>
2022-06-28 14:04:57 -0400

    Updates

---
+++ precache-manifest.814c9c3491ce3c9fecffd2f16f9ddcbd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "da60183d45aa1f7b4862",
+    "url": "/CompTimeApp/static/css/main.90e5ed49.chunk.css"
+  },
+  {
+    "revision": "da60183d45aa1f7b4862",
+    "url": "/CompTimeApp/static/js/main.e1638237.chunk.js"
+  },
+  {
+    "revision": "813669dc01277ce15ffa",
+    "url": "/CompTimeApp/static/js/runtime~main.19322d25.js"
+  },
+  {
+    "revision": "bbb311884a8766e4668f",
+    "url": "/CompTimeApp/static/js/2.5178fe95.chunk.js"
+  },
+  {
+    "revision": "5d06bb858cc186fdb179",
+    "url": "/CompTimeApp/static/js/3.b019360d.chunk.js"
+  },
+  {
+    "revision": "072e969bf7c78c0e155145acab2e8f0d",
+    "url": "/CompTimeApp/static/media/meditech-logo.072e969b.png"
+  },
+  {
+    "revision": "e28d50640437db01999ce5d6af5b1718",
+    "url": "/CompTimeApp/static/media/modalbutton.e28d5064.png"
+  },
+  {
+    "revision": "d43a3cacb144d293b53d433fa8303820",
+    "url": "/CompTimeApp/index.html"
+  }
+];
