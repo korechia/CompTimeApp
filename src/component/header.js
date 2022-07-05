11c98282c216c5402b27b46f8d4ea97510c36aba
korechia <35930077+korechia@users.noreply.github.com>
2022-07-05 10:49:28 -0400

    Update header.js

--- src/component/header.js
+++ src/component/header.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import "./Comptime.css";
 import logopic from "./meditech-logo.png";
 import Modalref from "./modalref.js"
+
 class Header extends Component {
   render() {
     return (
         <div className="header"><div className="logo"><img src={logopic} id="logo" /></div>
  <div className="wordH">Compensation Time Routine</div>
-<Modalref usernamestate={this.props.usernamestate} 
-changeUser = {this.props.changeUser}
-Authenticate={this.props.Authenticate}/>
+<Modalref usernamestate={this.props.usernamestate} />
 <div id="demo"></div>
 </div>
         )
