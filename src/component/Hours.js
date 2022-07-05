7857d276e8119141241ecc35d2d4b417d07c111e
korechia <35930077+korechia@users.noreply.github.com>
2022-07-05 14:59:43 -0400

    Update Hours.js

--- src/component/Hours.js
+++ src/component/Hours.js
@@ -4,16 +4,7 @@ import "./Comptime.css";
 class Hours extends Component {
 constructor(props) {
     super(props);
-    this.state ={
-    stime:"",
-    sdate:"",
-    edate:'',
-    etime:'',
-    ohour:''}
-  }
-
-handleChange=({target})=> {
-    this.setState({[target.name]: target.value});
+    this.state ={}
   }
 
   render() {
@@ -24,23 +15,7 @@ handleChange=({target})=> {
        <div class="Request">
        <h2> Request Hours off </h2>
        <div class="dropdown">
-    <label>Start Day </label>
-        <input type="date" name="sdate" value={this.state.sdate}
-         onChange={this.handleChange}/>
-         <label>                 Time</label>
-        <input type="time" name="stime" rvalue={this.state.stime}
-         onChange={this.handleChange}/>
-   <br/>
-<label>End Day </label>
-        <input type="date" name="edate" value={this.state.edate}
-         onChange={this.handleChange}/>
-        <label>                  Time</label>
-        <input type="time" name="etime" value={this.state.etime}
-         onChange={this.handleChange}/>
-         <br/>
-         <label> Number of hours to be removed </label>
-         <input type="number" name="ohour" value ={this.state.ohour}
-          onChange={this.handleChange}/>
+  
 </div>
 </div>
 </div>
