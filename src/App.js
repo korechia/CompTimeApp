6623e3622a593214c8399532bd68f6c852458730
korechia <35930077+korechia@users.noreply.github.com>
2022-07-05 15:34:31 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,6 +21,24 @@ export class App extends Component {
   componentDidMount(){
   this.changeUser("korechia","Kathleen",this.CalcHours)
 }
+    GetCurrentDate=()=>{
+    var today = new Date();
+    return this.formatDate(today);
+  }
+
+  formatDate(date){
+  let d = new Date(date);
+  let month = (d.getMonth() + 1).toString();
+  let day = d.getDate().toString();
+  let year = d.getFullYear();
+  if (month.length < 2) {
+    month = '0' + month;
+  }
+  if (day.length < 2) {
+    day = '0' + day;
+  }
+  return [year, month, day].join('-');
+}
 
     changeUser = (Uname,NameD,fun)=> {
       this.setState({user:Uname,
