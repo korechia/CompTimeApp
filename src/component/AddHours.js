eea0f1af9c902f910e025d54400442d1526a05f6
korechia <35930077+korechia@users.noreply.github.com>
2022-07-05 15:00:21 -0400

    Update AddHours.js

--- src/component/AddHours.js
+++ src/component/AddHours.js
@@ -4,16 +4,7 @@ import "./Comptime.css";
 class AddHours extends Component {
 	constructor(props) {
     super(props);
-    this.state ={
-    additionalHours:0,
-    stime:"",
-    sdate:"",
-    edate:'',
-    etime:''}
-  }
-  
-	handleChange=({target})=> {
-    this.setState({[target.name]: target.value});
+    this.state ={}
   }
 
 
@@ -25,23 +16,6 @@ class AddHours extends Component {
        <div class="Request">
        <h2> Add Hours Earned </h2>
        <div class="dropdown">
-    <label>Start Day </label>
-        <input type="date" name="sdate" value={this.state.sdate}
-         onChange={this.handleChange}/>
-         <label>                 Time</label>
-        <input type="time" name="stime" rvalue={this.state.stime}
-         onChange={this.handleChange}/>
-   <br/>
-<label>End Day </label>
-        <input type="date" name="edate" value={this.state.edate}
-         onChange={this.handleChange}/>
-        <label>                  Time</label>
-        <input type="time" name="etime" value={this.state.etime}
-         onChange={this.handleChange}/>
-         <br/>
-<label>Hours Earned </label>
-        <input type="number" name="additionalHours" value={this.state.additionalHours}
-         onChange={this.handleChange}/>
 </div>
 </div>
 </div>
