fbfe0f91a06525289c432e0e8e7d38e921649fde
korechia <35930077+korechia@users.noreply.github.com>
2022-07-11 09:22:48 -0400

    Update Signon.js

--- src/component/Signon.js
+++ src/component/Signon.js
@@ -12,7 +12,6 @@ import vacation from "./vacation.jpeg";
 class Signon extends Component {
 	constructor(props) {
     super(props);
-    this.state ={signin:false}
   }
  //$%$%$ NEW CODE! $%$% NEW CODE! $%$%$%$% NEW CODE! $%$%$%
 signin(){
@@ -23,27 +22,6 @@ console.log(provider);
 this.setState({signin:true},signInWithRedirect(auth, provider));
 }
 
-async componentDidMount(){
-	console.log("smount")
-	console.log(this.props.usernamestate.Authenticated)
-	if (this.state.signin){
-	const auth = getAuth();
- await getRedirectResult(auth).then((result) => {
-	console.log(result);
-	var email=result.user.email.substr(0, result.user.email.indexOf('@'));
-	this.props.changeUser(email,result.user.displayName,this.signcall,email);
-    // This gives you a Google Access Token. You can use it to access Google APIs.
- this.setState({signin:false})
-  }).catch((error) => {
-  	console.log(error.message);
-  	console.log(error.code);
-  	alert("e:"+error)
-   
-    // ...
-  });}else{
-  	console.log(getAuth());
-  }
-	}
  //END NEW CODE! $%$%$% END NEW CODE! $%$%$%$% END NEW CODE!
 	
   render() {
