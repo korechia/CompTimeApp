a0a72c2182cbfd361f2ad403ed27ff5318978c32
korechia <orechia_kathleen@wheatoncollege.edu>
2022-07-21 17:42:12 -0400

    app is now installable

--- src/index.js
+++ src/index.js
@@ -4,16 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//start service worker
- if (!navigator.serviceWorker.controller) {
-     navigator.serviceWorker.register("/sw.js").then(function(reg) {
-         console.log("Service worker has been registered for scope: " + reg.scope);
-     });
- }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  {(()=>{
+  	var link= window.location.href+'sw.js';
+  	console.log("inininininininininininini")
+  	console.log(window.location.href)
+    if (!navigator.serviceWorker.controller) {
+    	console.log("insideif")
+     navigator.serviceWorker.register(link).then((reg)=>{
+     	console.log(reg.scope);
+         console.log("Service worker has been registered for scope: " + reg.scope);
+     })
+     console.log("ouououououtuoutoutout");
+
+ }
+
+  })()}
+
     <App />
   </React.StrictMode>
 );
@@ -22,4 +32,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 //reportWebVitals();
-
